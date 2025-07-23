21f354e0b9e7d2645fa678f50371516a5673c148
theoramos1 <134008313+theoramos1@users.noreply.github.com>
2025-07-23 16:47:23 -0400

    feat: apply Brazilian masks for phone and postal code

--- mostrarAutorizacoes.php
+++ mostrarAutorizacoes.php
@@ -760,11 +760,10 @@ $pageUI->renderJS(); // Render the widgets' JS code
 ?>
 <?php if(Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL): ?>
 <script src="js/jQuery-Mask-Plugin-1.14.16/jquery.mask.min.js"></script>
+<script src="js/form-mask-utils.js"></script>
 <script>
 $(function(){
-    $('#telefone').mask('(00) 0000-0000');
-    $('#telemovel').mask('(00) 0 0000-0000');
-    $('#codigo_postal').mask('00000-000');
+    applyBrazilianMasks();
 });
 </script>
 <?php endif; ?>
--- mostrarPedidoInscricao.php
+++ mostrarPedidoInscricao.php
@@ -309,7 +309,7 @@ $db = new PdoDatabaseManager();
                 <div class="col-lg-4">
                 <div id="codigo_postal_div">
                   <label for="codigo_postal"><?= (Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL) ? 'CEP' : 'Código postal' ?></label>
-                  <input type="text" class="form-control" id="codigo_postal" name="codigo_postal" placeholder="<?= (Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL)?'xxxxx-xxx':'xxxx-xxx Localidade' ?>" onclick="verifica_codigo_postal()" onchange="verifica_codigo_postal()" value="<?php echo($submission['cod_postal']);?>" readonly required>
+                  <input type="text" class="form-control" id="codigo_postal" name="codigo_postal" placeholder="<?= (Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL)?'00000-000':'xxxx-xxx Localidade' ?>" onclick="verifica_codigo_postal()" onchange="verifica_codigo_postal()" value="<?php echo($submission['cod_postal']);?>" readonly required>
                   <span id="erro_postal_icon" class="glyphicon glyphicon-remove form-control-feedback" style="display:none;"></span>
                 </div>
                 </div>
@@ -749,11 +749,10 @@ $pageUI->renderJS(); // Render the widgets' JS code
 ?>
 <?php if(Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL): ?>
 <script src="js/jQuery-Mask-Plugin-1.14.16/jquery.mask.min.js"></script>
+<script src="js/form-mask-utils.js"></script>
 <script>
 $(function(){
-    $('#telefone').mask('(00) 0000-0000');
-    $('#telemovel').mask('(00) 0 0000-0000');
-    $('#codigo_postal').mask('00000-000');
+    applyBrazilianMasks();
 });
 </script>
 <?php endif; ?>
@@ -798,7 +797,7 @@ function validar()
         
         if(!codigo_postal_valido(cod_postal, '<?= Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) ?>'))
         {
-                alert("O código postal que introduziu é inválido. Deve ser da forma '<?= (Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL)?'xxxxx-xxx':'xxxx-xxx Localidade' ?>'.");
+                alert("O código postal que introduziu é inválido. Deve ser da forma '<?= (Configurator::getConfigurationValueOrDefault(Configurator::KEY_LOCALIZATION_CODE) == Locale::BRASIL)?'00000-000':'xxxx-xxx Localidade' ?>'.");
                 return false;
         }
                 
