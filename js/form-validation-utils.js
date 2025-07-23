1d803f20da90605a3f3313c3929ff141d2df26a5
theoramos1 <134008313+theoramos1@users.noreply.github.com>
2025-07-23 17:43:52 -0400

    Update phone regex

--- js/form-validation-utils.js
+++ js/form-validation-utils.js
@@ -7,8 +7,8 @@ function telefone_valido(num, locale)
     }
     else if(locale === "BR")
     {
-        const mobile = /^(\+\d{1,}[-\s]{0,1})?\s*\(?(\d{2}|\d{0})\)?[-. ]?9\d{4}[-. ]?\d{4}\s*$/;
-        const landline = /^(\+\d{1,}[-\s]{0,1})?\s*\(?(\d{2}|\d{0})\)?[-. ]?\d{4}[-. ]?\d{4}\s*$/;
+        const mobile = /^\(\d{2}\) 9 \d{4}-\d{4}$/;
+        const landline = /^\(\d{2}\) \d{4}-\d{4}$/;
         return mobile.test(num) || landline.test(num);
     }
 
