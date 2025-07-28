7eec86b09fec35f74e351e6da3f81b47af375852
theoramos1 <134008313+theoramos1@users.noreply.github.com>
2025-07-28 14:34:05 -0400

    Fix undefined Configurator in brazilian masks

--- includes/brazilian_masks.php
+++ includes/brazilian_masks.php
@@ -1,6 +1,13 @@
 <?php
 // Include jQuery mask plugin and apply default Brazilian input masks when locale is BR
 
+// Load dependencies in case they weren't required by the including script
+require_once(__DIR__ . '/../core/Configurator.php');
+require_once(__DIR__ . '/../core/domain/Locale.php');
+
+use catechesis\Configurator;
+use core\domain\Locale;
+
 // Optionally specify $maskPathPrefix before including this file to adjust the
 // relative path of the JS files. Default is an empty string which works for
 // files in the project root.
